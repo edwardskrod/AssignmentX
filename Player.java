6f8b43247ade751d816d87666dfe424fcdc09589
Edward J Skrod <ejs09f@my.fsu.edu>
2014-04-02 06:38:44 -0400

    Worked on Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,49 @@
+import javax.swing.JOptionPane;
 
 public class Player {
 
+	private String playerName;
+	private int playerAccountBalance;
+	
+	
+	// Default Constructor
+	Player ( ) {
+		
+		playerName = "";
+		playerAccountBalance = 0;
+		
+	}
+
+	Player (String name, int balance ) {
+		
+		playerName = name;
+		playerAccountBalance = balance;
+	
+	}
+		
+	public String getPlayerName( ) {
+		return playerName;
+	}
+	
+	public void setPlayerName( String name ) {
+		playerName = name;
+	}
+	
+	public int getPlayerAccountBalance ( ) {
+		return playerAccountBalance;
+	}
+	
+	public void setPlayerAccountBalance ( int balance ) {
+		playerAccountBalance = balance;
+	}
+	
+	public static String promptForPlayerName( ) 
+	// When this function is called, a JOptionPane Dialog Box will open 
+	// and ask the player to enter their name
+	{
+		String name = JOptionPane.showInputDialog("Please enter your name");
+		return name;
+	}
+	
+	
 }
