6afc639dc33a960d4a9abb897c4e86384bc7a7e8
Edward J Skrod <ejs09f@my.fsu.edu>
2014-04-04 07:35:02 -0400

    Update to main()
    Added while loop to prompt user to spin or exit game

--- SlotMachine.java
+++ SlotMachine.java
@@ -1,4 +1,5 @@
 // entrance to program
+import java.util.Scanner;
 
 public class SlotMachine {
 	
@@ -12,14 +13,28 @@ public class SlotMachine {
 		// If not, set initial account balance to 100
 		FileHandler.getAccountInformation(thePlayer);
 		
+		
+		// Instantiate a new Board
+		Board board = new Board();
+		
 		// Play the game
-/*		
-		Board b = new Board();
-		// We're going to have to pass the accountBalance in to spin()
-		SlotMachineImpl.spin(b);
-		b.printBoardWithTokens();
-
-*/		
+		Scanner userChoice = new Scanner (System.in);
+		System.out.println("Press 'S' to Spin and 'X' to exit: ");
+		String c = userChoice.next();
+		
+		while (!c.equals("x") && !c.equals("X")){			// While the player has not exited the game 
+		
+			if ( c.equals("S") || c.equals("s") ) {
+		 
+				SlotMachineImpl.spin(board);
+				board.printBoardWithTokens();
+			} // end if
+			
+			// Prompt user again
+			System.out.println("\nPress 'S' to Spin and 'X' to exit: ");
+			 c = userChoice.next();
+		} // end while
+		
 		// For testing;  Pretend there were winnings
 		thePlayer.setPlayerAccountBalance(12999);
 		
