afb515435fa5e57a965cfc6a44f1ae6c46ef200e
Edward J Skrod <ejs09f@my.fsu.edu>
2014-04-17 14:17:52 -0400

    Updated gridbagframe

--- SlotMachineGridBagFrame.java
+++ SlotMachineGridBagFrame.java
@@ -4,7 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.Component;
 //import javax.swing.JButton;
 import javax.swing.JFrame;
-//import javax.swing.JPanel;
+import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -22,7 +22,7 @@ public class SlotMachineGridBagFrame extends JFrame
 
 	private GridBagLayout slotMachineLayout;  	// layout of this frame
 	private GridBagConstraints constraints;  	// constraints of the layout
-	private JLabel [] labels = new JLabel[ 36 ];;
+	private JLabel [] labels = new JLabel[ 64 ];;
 	
 	public SlotMachineGridBagFrame ( )
 	{
@@ -60,17 +60,28 @@ public class SlotMachineGridBagFrame extends JFrame
 		
 		constraints = new GridBagConstraints( );
 		
-		// Add the icons 1 - 36   25.9 Layout Managers: BoxLayout and GridBagLayout 1035
-		Icon icon1= new ImageIcon ( getClass().getResource( "1.jpg" ));
+		// Add the icons 1 - 64   25.9 Layout Managers: BoxLayout and GridBagLayout 1035
+		//Icon icon1 = new ImageIcon ( getClass().getResource( "1.jpg" ));
 		
 		// put the icons into Jlabels
 		//label1 = new JLabel();
 		//label1.setIcon( icon1 );
 		
 		// Populate the array of lables with Icons.  Icons are named 1 - 36.jpg
-		for (int lcv = 0; lcv < 36; ++lcv ){
+		for (int lcv = 0; lcv < 64; ++lcv ){
+			String name;
+			if (lcv < 9){
+				
+				name = "slotMachine_0" + (lcv + 1) + ".jpg";
+				System.out.println(name);
+			}
+			else {
+				name = "slotMachine_" + (lcv + 1) + ".jpg";
+				System.out.println(name);
+
+				
+			}
 			
-			String name = (lcv + 1) + ".jpg";
 		    Icon icon = new ImageIcon (getClass().getResource( name ));
 			labels[lcv] = new JLabel();
 			labels[lcv].setIcon( icon );
@@ -79,12 +90,12 @@ public class SlotMachineGridBagFrame extends JFrame
 		
 		constraints.fill = GridBagConstraints.BOTH;
 		int y;
-		int num = 0;  // num will track the total number of icons from 0 - 35
-		for (int i = 0; i < 6; ++i ) {
+		int num = 0;  // num will track the total number of icons from 0 - 64
+		for (int i = 0; i < 8; ++i ) {
 		
 			y = 0;
 			
-			for (int j = 0; j < 6; ++j ){
+			for (int j = 0; j < 8; ++j ){
 				
 				addComponent ( labels[ num ], i, j, 1, 1);
 				++num;
@@ -123,8 +134,9 @@ public class SlotMachineGridBagFrame extends JFrame
 		SlotMachineGridBagFrame gridBagFrame = new SlotMachineGridBagFrame ();
 		
 		gridBagFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		gridBagFrame.setSize( 810, 810 );
+		gridBagFrame.setSize( 1000, 1000 );
 		gridBagFrame.setVisible( true );
+		
 	}
 	
 	
