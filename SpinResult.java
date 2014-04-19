900f25492862ee3a2214c59fd92472c9482e52d5
jessehanna <jessehanna@tampabay.rr.com>
2014-04-19 08:52:35 -0400

    changed SpinResult

--- SpinResult.java
+++ SpinResult.java
@@ -31,63 +31,64 @@ $1300
 
 
 public enum SpinResult {
+//	new ImageIcon (getClass().getResource( "Java-bug.jpg" ))
 
-	JAVA("JAVA", 0, 1, new ImageIcon ("Java-bug.jpg"), new ImageIcon ("java-bugBlur.jpg")),
-	LUCKY_SEVEN("LUCKY7", 2, 3, 4, new ImageIcon ("7.jpg"), new ImageIcon ("7Blur.jpg")), 
-	BAR("BAR", 5, 6, 7, 8, 9, 10, new ImageIcon ("bar.jpg"), new ImageIcon ("barBlur.jpg")), 
-	BAR_BAR("BARBAR", 11, 12, 13, 14, 15, new ImageIcon ("barbar.jpg"), new ImageIcon ("barbarBlur.jpg")), 
-	BAR_BAR_BAR("BARBARBAR", 16, 17, 18, 19, new ImageIcon ("barbarbar.jpg"), new ImageIcon ("barbarbarBlur.jpg")), 
-	BLANK("BLANK", 20, 21, 22, 23, new ImageIcon ("BLANK.jpg"), new ImageIcon ("BLANK.jpg")), 
+	JAVA("JAVA", 0, 1, "Java-bug.jpg", "java-bugBlur.jpg"),
+	LUCKY_SEVEN("LUCKY7", 2, 3, 4, "7.jpg", "7Blur.jpg"), 
+	BAR("BAR", 5, 6, 7, 8, 9, 10, "bar.jpg", "barBlur.jpg"), 
+	BAR_BAR("BARBAR", 11, 12, 13, 14, 15, "barbar.jpg", "barbarBlur.jpg"), 
+	BAR_BAR_BAR("BARBARBAR", 16, 17, 18, 19, "barbarbar.jpg", "barbarbarBlur.jpg"), 
+	BLANK("BLANK", 20, 21, 22, 23, "BLANK.jpg", "BLANK.jpg"), 
 	UNKNOWN("UNKNOWN");
 
 	private int spinResult1, spinResult2, spinResult3, spinResult4,
 			spinResult5, spinResult6;
-	private Icon icon, blurredIcon;
+	private String icon, blurredIcon;
 	private String name;
 	private static final int IMPOSSIBLE_MATCH = 999;
 
 	// constructors
 	SpinResult(String name) {
 		setSpin(name, IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH,
-				IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, null,
-				null);
+				IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, "",
+				"");
 	}
 
-	SpinResult(String name, int spinResult1, int spinResult2, Icon icon,
-			Icon blurredIcon) {
+	SpinResult(String name, int spinResult1, int spinResult2, String icon,
+			String blurredIcon) {
 		setSpin(name, spinResult1, spinResult2, IMPOSSIBLE_MATCH,
 				IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, icon,
 				blurredIcon);
 	}
 
 	SpinResult(String name, int spinResult1, int spinResult2, int spinResult3,
-			Icon icon, Icon blurredIcon) {
+			String icon, String blurredIcon) {
 		setSpin(name, spinResult1, spinResult2, spinResult3, IMPOSSIBLE_MATCH,
 				IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, icon, blurredIcon);
 	}
 
 	SpinResult(String name, int spinResult1, int spinResult2, int spinResult3,
-			int spinResult4, Icon icon, Icon blurredIcon) {
+			int spinResult4, String icon, String blurredIcon) {
 		setSpin(name, spinResult1, spinResult2, spinResult3, spinResult4,
 				IMPOSSIBLE_MATCH, IMPOSSIBLE_MATCH, icon, blurredIcon);
 	}
 
 	SpinResult(String name, int spinResult1, int spinResult2, int spinResult3,
-			int spinResult4, int spinResult5, Icon icon, Icon blurredIcon) {
+			int spinResult4, int spinResult5, String icon, String blurredIcon) {
 		setSpin(name, spinResult1, spinResult2, spinResult3, spinResult4,
 				spinResult5, IMPOSSIBLE_MATCH, icon, blurredIcon);
 	}
 
 	SpinResult(String name, int spinResult1, int spinResult2, int spinResult3,
-			int spinResult4, int spinResult5, int spinResult6, Icon icon,
-			Icon blurredIcon) {
+			int spinResult4, int spinResult5, int spinResult6, String icon,
+			String blurredIcon) {
 		setSpin(name, spinResult1, spinResult2, spinResult3, spinResult4,
 				spinResult5, spinResult6, icon, blurredIcon);
 	}
 
 	private void setSpin(String name, int spinResult1, int spinResult2,
 			int spinResult3, int spinResult4, int spinResult5, int spinResult6,
-			Icon icon, Icon blurredIcon) {
+			String icon, String blurredIcon) {
 		this.name = name;
 		this.spinResult1 = spinResult1;
 		this.spinResult2 = spinResult2;
@@ -95,17 +96,21 @@ public enum SpinResult {
 		this.spinResult4 = spinResult4;
 		this.spinResult5 = spinResult5;
 		this.spinResult6 = spinResult6;
+		this.icon = icon;
+		this.blurredIcon = blurredIcon;
+//		this.icon = new ImageIcon (getClass().getResource( icon ));
+//		this.blurredIcon = new ImageIcon (getClass().getResource( blurredIcon ));
 	}
 
 	public String getSpinName() {
 		return this.name;
 	}
 
-	public Icon getIcon() {
+	public String getIcon() {
 		return this.icon;
 	}
 
-	public Icon getBlurredIcon() {
+	public String getBlurredIcon() {
 		return this.blurredIcon;
 	}
 
